0b66dd9edac6bcb2af3a4718ecbb4b7da7fed44d
Nalindadsn <nalindadsn@gmail.com>
2025-05-06 19:33:46 +0530

    add data2

--- src/components/header/nav/Footer/index.jsx
+++ src/components/header/nav/Footer/index.jsx
@@ -2,9 +2,12 @@ import styles from "./style.module.scss";
 
 export default function index() {
   return (
-    <div className={styles.footer}>
-      <a>Facebook</a>
-      <a>LinkedIn</a>
+    <div className="mt-5 text-center">
+      <div className={`${styles.footer} `}>
+        <a>Facebook</a>
+        <a>LinkedIn</a>
+      </div>
+      <div className="border-t border-white/[0.2]">+94704238939</div>
     </div>
   );
 }
