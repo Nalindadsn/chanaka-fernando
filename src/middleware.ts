5a26c03d52d3ee0e51eab273c5e4c2abfeddb81b
Nalindadsn <nalindadsn@gmail.com>
2025-05-06 18:47:35 +0530

    add data2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,12 @@
-import { createRouteMatcher, auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/blog(.*)",
-  "/about",
-  "/contact",
-  "/api/contact",
-  "/api/webhook/clerk",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-]);
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL("/home", request.url));
+}
 
-const isProtectedRoute = createRouteMatcher(["/admin(.*)", "/user(.*)"]);
-
+// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
+  matcher: "/about/:path*",
 };
