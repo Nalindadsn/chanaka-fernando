232a91b222eef8e02827a50b77e43047770d125c
Nalindadsn <nalindadsn@gmail.com>
2025-05-07 01:18:02 +0530

    add data2

--- src/app/MoreWorks.tsx
+++ src/app/MoreWorks.tsx
@@ -1,30 +1,33 @@
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function MoreWorks() {
   return (
-    <div>
-      <div className="max-w-screen-xl mx-auto py-8 px-4 lg:py-16 lg:px-6">
+    <div className="bg-white dark:bg-black text-gray-900 dark:text-gray-100">
+      <div className="container mx-auto py-8 px-4 lg:py-16 lg:px-6">
         <div className="text-center mb-10">
           <h2 className="text-4xl tracking-tight font-bold text-primary-800">
-            Highlighted Features
+            My Works
           </h2>
         </div>
 
         <div className="flex flex-col md:flex-row">
           <div className="mr-0 md:mr-8 mb-6 md:mb-0">
-            <img
-              className="w-1/2 md:w-full mx-auto"
-              src="https://placeholder.pics/svg/400"
-              alt="can_help_banner"
+            <Image
+              className="w-[400px] h-[400px] md:w-full mx-auto"
+              src="/projects.png"
+              alt="projects"
+              width={400}
+              height={400}
             />
           </div>
 
           <div className="flex-1 flex flex-col sm:flex-row flex-wrap -mb-4 -mx-2">
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
-              <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                <h3 className="text-2xl font-bold text-md mb-6">
-                  Dynamic Personalization:
-                </h3>
+              <div className="h-full py-4 px-6  rounded-br-xl">
+                <h3 className="text-2xl font-bold text-md mb-6">EzQuizy</h3>
                 <p className="text-sm">
                   Our platform leverages user data and behavior to provide a
                   highly personalized experience, with dynamic content and
@@ -33,7 +36,7 @@ function MoreWorks() {
               </div>
             </div>
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
-              <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
+              <div className="h-full py-4 px-6  rounded-br-xl">
                 <h3 className="text-2xl font-bold text-md mb-6">
                   Mobile-Optimized Interface
                 </h3>
@@ -47,7 +50,7 @@ function MoreWorks() {
             </div>
 
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
-              <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
+              <div className="h-full py-4 px-6  rounded-br-xl">
                 <h3 className="text-2xl font-bold text-md mb-6">
                   24/7 Customer Support
                 </h3>
@@ -61,15 +64,14 @@ function MoreWorks() {
             </div>
 
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
-              <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                <h3 className="text-2xl font-bold text-md mb-6">
-                  Secure Payment Processing
-                </h3>
-                <p className="text-sm">
-                  We use cutting-edge security measures to protect our
-                  customers' sensitive information and ensure the safety of all
-                  transactions made on our site.
-                </p>
+              <div className="h-full py-4 px-6  rounded-br-xl flex items-center justify-center">
+                <Link
+                  href="/work"
+                  className="text-gray-900 inline-flex items-center gap-2 items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center dark:text-white rounded-lg bg-primary-700 hover:text-orange-500 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                >
+                  More Works
+                  <ArrowRight />
+                </Link>
               </div>
             </div>
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,16 +21,7 @@ function page() {
           <TechStack />
         </Suspense>
       </div>
-      <div className="py-10 mt-20 bg-gray-900 border-t border-black shadow-md ">
-        <div className="md:max-w-6xl mx-auto">
-          <h2 className="text-3xl font-bold text-white mb-4">Work with Us</h2>
-          <p className="text-gray-300">
-            We are always looking for talented individuals to join our team. If
-            you have a passion for coding and a drive to make a positive impact,
-            we invite you to apply.
-          </p>
-        </div>
-      </div>
+
       <div>
         <MoreWorks />
       </div>
