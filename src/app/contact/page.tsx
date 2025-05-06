a5e598d618690be4edfd7fec1d1b6691432c283c
Nalindadsn <nalindadsn@gmail.com>
2025-05-06 21:54:08 +0530

    add data2

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -64,11 +64,11 @@ const ContactPage = () => {
 
   return (
     <main className="min-h-screen bg-white dark:bg-black/[0.96] antialiased bg-grid-black/[0.02] dark:bg-grid-white/[0.02]">
-      <section className="bg-white dark:bg-gray-900 relative pt-14">
+      <section className="bg-white dark:bg-gray-900 relative pt-14 overflow-hidden">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Nalinda Dissanayaka
+              Contact Me
             </h1>
             <div className="bg-orange-500 p-1  w-[100px]"></div>
 
@@ -80,17 +80,22 @@ const ContactPage = () => {
               Home
             </Link>
             <Link
-              href="/work"
-              className="z-100inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              href="/contact"
+              className="z-100 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
-              My Works
+              Contact Me
             </Link>
           </div>
           <div className=" lg:mt-0 lg:col-span-5 lg:flex">
             <p className="right-0 absolute -bottom-10 max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              <div className="bg-orange-500 px-2 absolute bottom-1 -left-15 z-100 whitespace-nowrap text-sm text-white rounded-full">
-                Full Stack Developer
+              <div className="absolute bottom-1 -left-15 z-100">
+                {" "}
+                <span className="text-sm text-sm">Nalinda Dissanayaka</span>
+                <div className="bg-orange-500 px-2  whitespace-nowrap text-sm text-white rounded-full">
+                  Full Stack Developer
+                </div>
               </div>
+
               <Image
                 src="/nalinda-dissanayaka.png"
                 alt="img"
