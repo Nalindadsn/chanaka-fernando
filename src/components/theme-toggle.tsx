3ba5e634c6c26e1ed9a34b0a1e339f2d5463da64
Nalindadsn <nalindadsn@gmail.com>
2025-05-07 10:23:11 +0530

    add data2

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -20,7 +20,7 @@ export function ThemeToggle() {
       <DropdownMenuTrigger asChild>
         <Button
           size="icon"
-          className="relative h-[1.5rem] w-[1.5rem] border border-white text-white bg-black ring-offset-gray-500 focus:ring-offset-0 onfocus:ring-0"
+          className="relative  text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 border border-white"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
