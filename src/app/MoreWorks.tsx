16f607bf7aec402567cc79519dca9c345f6641a8
Nalindadsn <nalindadsn@gmail.com>
2025-08-24 15:33:13 +0530

    add

--- src/app/MoreWorks.tsx
+++ src/app/MoreWorks.tsx
@@ -8,27 +8,30 @@ function MoreWorks() {
   return (
     <div className="bg-white dark:bg-black text-gray-900 dark:text-gray-100">
       <div className="container mx-auto py-8 px-4 lg:py-16 lg:px-6">
-        <div className="text-center mb-10">
-          <h2 className="text-4xl tracking-tight font-bold text-primary-800">
-            My Works
+        <div className="text-center mb-5">
+          <h2 className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-950 to-neutral-500 dark:from-neutral-50 dark:to-neutral-400">
+            Posts
           </h2>
+          <p className="mt-4 text-xl text-neutral-600 dark:text-neutral-400">
+            latest activities
+          </p>
         </div>
 
         <div className="flex flex-col md:flex-row">
           <div className="mr-0 md:mr-8 mb-6 md:mb-0">
             <Image
               className="w-[400px] h-[400px] md:w-full mx-auto"
-              src="/projects.png"
+              src="/c.png"
               alt="projects"
-              width={400}
-              height={400}
+              width={431}
+              height={360}
             />
           </div>
 
           <div className="flex-1 flex flex-col sm:flex-row flex-wrap -mb-4 -mx-2">
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
               <div className="relative h-full py-4 px-6  rounded-br-xl">
-                <h3 className="absolute top-2 left-4 text-sm font-bold text-md mb-6 bg-orange-500 px-2 py-1 text-white rounded-md">
+                <h3 className="absolute top-2 left-4 text-sm font-bold text-md mb-6 bg-green-600 px-2 py-1 text-white rounded-md">
                   EzQuizy
                 </h3>
                 <div className="flex gap-1 absolute bottom-8 right-10">
@@ -55,7 +58,7 @@ function MoreWorks() {
             </div>
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
               <div className="relative h-full py-4 px-6  rounded-br-xl">
-                <h3 className="absolute top-2 left-4 text-sm font-bold text-md mb-6 bg-orange-500 px-2 py-1 text-white rounded-md">
+                <h3 className="absolute top-2 left-4 text-sm font-bold text-md mb-6 bg-green-500 px-2 py-1 text-white rounded-md">
                   Gold Mortgaging Project
                 </h3>
                 <div className="flex gap-1 absolute bottom-8 right-10">
@@ -99,7 +102,7 @@ function MoreWorks() {
               <div className="h-full py-4 px-6  rounded-br-xl flex items-center justify-center">
                 <Link
                   href="/works"
-                  className="text-gray-900 inline-flex items-center gap-2 items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center dark:text-white rounded-lg bg-primary-700 hover:text-orange-500 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                  className="text-gray-900 inline-flex items-center gap-2 items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center dark:text-white rounded-lg bg-primary-700 hover:text-green-500 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
                 >
                   More Works
                   <ArrowRight />
