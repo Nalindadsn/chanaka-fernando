a5f4577b55c167eb8dc2f7bcef7a2aa58c3a8af5
Nalindadsn <nalindadsn@gmail.com>
2025-09-05 11:37:08 -0700

    menu

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -20,7 +20,7 @@ export function ThemeToggle() {
       <DropdownMenuTrigger asChild>
         <Button
           size="icon"
-          className="z-100 relative  text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 border border-white"
+          className="z-100 relative  text-white bg-neutral-800 hover:bg-neutral-900 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-neutral-800 dark:hover:bg-neutral-700  border border-white"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -29,7 +29,7 @@ export function ThemeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent
         align="end"
-        className="bg-white dark:bg-gray-900 dark:border-gray-800 z-[10000]"
+        className="bg-white dark:bg-neutral-900 dark:border-neutral-800 z-[10000]"
       >
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
