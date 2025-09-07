40455e6eec70c96adc39e5b15470a69284df827c
Nalindadsn <nalindadsn@gmail.com>
2025-09-06 19:31:52 -0700

    add

--- src/components/testimonials/js/EmblaCarousel.tsx
+++ src/components/testimonials/js/EmblaCarousel.tsx
@@ -30,21 +30,36 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
     onPrevButtonClick,
     onNextButtonClick,
   } = usePrevNextButtons(emblaApi);
-
+const testiminials=[
+  {
+    name:"Anna P",
+    position:"Cooking Class Participant",
+    image:"/testiminial/1.jpg",
+    rating:5,
+    feedback:"Chef Chanaka made the cooking lecture both fun and educational. The way complex techniques were explained in simple steps kept everyone engaged. I left not only with new skills but also with the confidence to try them at home. Truly inspiring!"
+  },
+  {
+    name:"David Smith ",
+    position:"Corporate Event Client",
+    image:"/testiminial/2.jpg",
+    rating:5,
+    feedback:"Working with 3Flavours Jet Catering was an absolute pleasure. From the planning stage to the final service, everything was handled with professionalism and care. The food was outstanding—fresh, flavorful, and beautifully presented. Our guests couldn’t stop talking about the experience. Truly a first-class catering service!"
+  },
+]
   return (
     <section className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          {slides.map((index) => (
+          {testiminials.map((post,index) => (
             <div className="embla__slide" key={index}>
               <div className="embla__slide__number text-center">
                 <div className="">
 <div className="w-full flex justify-center">
   {
-    index===0 ?
+    post?.image ?
     <Image
-                    src="/testiminial1.jpg"
-                    alt="img"
+                    src={post?.image}
+                    alt="testiminial"
                     width={100}
                     height={100}
                     className=" relative z-10 w-[80px]  h-[80px] rounded-full border-4 border-white shadow-lg mb-3"
@@ -54,13 +69,6 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
   }
   
   
-                  {/* <Image
-                    src="/gold-medal.png"
-                    alt="img"
-                    width={192}
-                    height={324}
-                    className=" relative z-10 w-[80px] "
-                  /> */}
                 </div>
                 <div className="text-sm font-normal  text-neutral-300 ">
                   <div className="flex w-full justify-center">
@@ -70,12 +78,11 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
                     <IconStarFilled className="text-yellow-400 w-4 h-4"/>
                     <IconStarFilled className="text-yellow-400 w-4 h-4"/>
                   </div>
-                  <h2 className="text-2xl">Mr Bandara Dissanayaka.</h2>
-                  <h2 className="text-sm underline ">Excecutive Chef</h2>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus, adipisci molestiae ducimus fugiat omnis veritatis
-                  quo, non dignissimos fugit dolor rem et! Culpa sunt facere
-                  distinctio id quae commodi. Iure!
+                  <h2 className="text-2xl">{post?.name}</h2>
+                  <h2 className="text-sm underline ">{post?.position}</h2>
+                  <p>
+                    {post?.feedback}
+                  </p>
                 </div>
                 </div>
                 
